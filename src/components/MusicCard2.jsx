ce2c4f95c1f7d2d194530ab4c68b850de73c9297
Leonardo Martins <leonardogustavo@outlook.com>
2023-04-19 20:13:38 -0300

    Requisito 8 passou

--- src/components/MusicCard2.jsx
+++ src/components/MusicCard2.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
 import Loading from './Loading';
 
 export default class MusicCard extends Component {
   state = {
     favoriteLoading: false,
     listOfFavoritedSongs: [],
+    checkedItems: {},
   };
 
   onInputChange = ({ target }) => {
@@ -18,27 +19,62 @@ export default class MusicCard extends Component {
     });
   };
 
+  // checkedValidation = (nomeDaMusicaAtual) => {
+  //   const { listOfFavoritedSongs } = this.state;
+  //   return listOfFavoritedSongs
+  // };
+
   handleFavCheckbox = async (element) => {
     const nomeDaMusicaAtual = element.trackName;
     this.setState({
       favoriteLoading: true,
 
     });
-    this.setState((prevState) => ({
-      listOfFavoritedSongs: [...prevState.listOfFavoritedSongs, nomeDaMusicaAtual],
-    }));
 
-    const addToFavListAPI = await addSong(element);
+    const { checkedItems } = this.state;
+    const isChecked = !checkedItems[element.trackId];
+    const newCheckedItems = {
+      ...checkedItems,
+      [element.trackId]: isChecked,
+    };
+    this.setState({ checkedItems: newCheckedItems });
+
+    if (isChecked) {
+      this.setState((prevState) => ({
+        listOfFavoritedSongs: [...prevState.listOfFavoritedSongs, nomeDaMusicaAtual],
+      }));
+      await addSong(element);
+    } else {
+      const { listOfFavoritedSongs } = this.state;
+      const index = listOfFavoritedSongs.indexOf(nomeDaMusicaAtual);
+      const newList = [
+        ...listOfFavoritedSongs.slice(0, index),
+        ...listOfFavoritedSongs.slice(index + 1),
+      ];
+      this.setState({ listOfFavoritedSongs: newList });
+      await removeSong(element);
+    }
 
     this.setState({
       favoriteLoading: false,
     });
-
-    return addToFavListAPI;
   };
 
+  //   this.setState((prevState) => ({
+  //     listOfFavoritedSongs: [...prevState.listOfFavoritedSongs, nomeDaMusicaAtual],
+  //   }), this.checkedValidation(nomeDaMusicaAtual));
+
+  //   const addToFavListAPI = await addSong(element);
+
+  //   this.setState({
+  //     favoriteLoading: false,
+  //   });
+
+  //   return addToFavListAPI;
+  // };
+
   render() {
-    const { favoriteLoading, listOfFavoritedSongs } = this.state;
+    const { favoriteLoading, checkedItems } = this.state;
     const { songsList } = this.props;
     return (
       <div>
@@ -66,10 +102,10 @@ export default class MusicCard extends Component {
                     <input
                       key={ element.trackName }
                       type="checkbox"
-                      name="isFavorite"
-                      id="isFavorite"
+                      name={ `isFavorite-${element.trackId}` }
+                      id={ `isFavorite-${element.trackId}` }
                       onChange={ this.onInputChange }
-                      checked={ listOfFavoritedSongs.includes(element.trackName) }
+                      checked={ checkedItems[element.trackId] }
                       data-testid={ `checkbox-music-${element.trackId}` }
                       onClick={ () => this.handleFavCheckbox(element) }
                     />
